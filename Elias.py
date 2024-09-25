17aea7358a91553cd6baed0dc5d5c18818e4ccdb
Eliasdl007 <eliasjose0798@gmail.com>
2024-09-25 18:29:11 +0000

    elias

--- Elias.py
+++ Elias.py
@@ -3,6 +3,9 @@ os.system("cls || clear")
 
 print("lista pratos escolhidos: ")
 for prato in pratos_escolhidos:
-    print(f"Codigo:{prato["codigo"]}, Nome: {prato["nome"]}")
+    print(f"pratos escolhidos")
 
-print(f"f")
+print(f"\nValor do pratos:{valor_pratos}")
+print(f"\nForma de Pagamento:{forma_pagamento}")
+print(f"\nValor do desconto:10% {desconto:}")
+print(f"\nValor final:{valor_fina}")
