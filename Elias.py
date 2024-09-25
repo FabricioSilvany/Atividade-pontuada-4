880da28ca1bc06cedd9d1792777abc1cc41bf538
Eliasdl007 <eliasjose0798@gmail.com>
2024-09-25 17:55:24 +0000

    elias

--- Elias.py
+++ Elias.py
@@ -1,4 +1,8 @@
 import os
-os.system("cls || clear" )
+os.system("cls || clear")
 
-print("elias monitorando")
+print("lista pratos escolhidos: ")
+for prato in pratos_escolhidos:
+    print(f"Codigo:{prato["codigo"]}, Nome: {prato["nome"]}")
+
+print(f"f")
