67e085e02e66576ddccbcbadd3cd4c0027b7922c
Iron-Holster <victorandradecostapinto@gmail.com>
2024-09-25 16:51:00 +0000

    Save

---
+++ Victor.py
@@ -0,0 +1,17 @@
+import os
+
+os.system("cls || clear")
+
+# Defs para o pagamento
+
+def vista (valor_pratos):
+    #Pagamento a vista recebe 10% de desconto
+    valor_final = valor_pratos - (0.10 * valor_pratos)
+    valor_descontado = (0.10 * valor_pratos)
+    return valor_final, valor_descontado
+def cartao (valor_pratos):
+    #pagamento com cartão recebe 10% 
+    valor_final = valor_pratos + (0.10 * valor_pratos)
+    valor_adicionado = (0.10 * valor_pratos)
+    return valor_final, valor_adicionado
+
